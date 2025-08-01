f09d1ca385a7dcac2fbd733b8b75891978f9b605
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2025-08-01 17:21:50 +0300

    DateToNewYears

--- JavaScript/ArrayOOPString/date-practice.js
+++ JavaScript/ArrayOOPString/date-practice.js
@@ -1,7 +1,23 @@
 "use strict"
+
 const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds");
 
-let days = new Date().getDate();
-console.log(days);
 
-daysEl.textContent = days;
+function countdown()
+{
+    const currnetDate = new Date();
+    const currentYear = currnetDate.getFullYear();
+    const newYearDate = new Date(`January 1, ${currentYear + 1}, 00:00:00`);
+    
+    const timeDifference = newYearDate - currnetDate;
+    
+    secondsEl.innerText = Math.floor(timeDifference / 1000);
+    minutesEl.innerText = Math.floor(timeDifference / (1000 * 60));
+    hoursEl.innerText = Math.floor(timeDifference / (1000 * 60 * 60));
+    daysEl.innerText = Math.floor(timeDifference / (1000  * 60 * 60 * 24));
+}
+
+countdown();
