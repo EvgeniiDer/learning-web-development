d53a144107697076a547b59a9eb6fd95f12536bb
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2025-08-02 11:49:19 +0300

    Exr:ColorButton Done

--- JavaScript/Buttons/js/main.js
+++ JavaScript/Buttons/js/main.js
@@ -2,8 +2,9 @@ import ColorButton from "./ColorButton.js";
 import MyButton from "./MyButton.js";
 
 
-let baseButton = new MyButton("BaseButton", "Two");
-baseButton.show();
 
-let btn = new ColorButton("ColorButton", "Two", "Thre");
-btn.show();
+let btnGreen = new ColorButton("Text", "btnCss", "green");
+btnGreen.show();
+
+let btnRed = new ColorButton("Red", "btnCss", "red");
+btnRed.show();
